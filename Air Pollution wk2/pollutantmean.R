c7dc0515976cdca606dc059f7408b8f1b1d2ba68
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2015-05-27 14:45:25 -0400

    temp does not work when field is input from function, need to work

--- Air Pollution wk2/pollutantmean.R
+++ Air Pollution wk2/pollutantmean.R
@@ -3,13 +3,36 @@ pollutantmean <- function(directory, pollutant, id) {
   ## the location of the CSV files
   
   #Diretory where all files are stored
-  #directory <- "C:/Users/ks692/Documents/Dingchao/Courses/R/Assignments/Air Pollution wk2/specdata/"
+  directory <- "C:/Users/ks692/Documents/Dingchao/Courses/R/specdata/"
   
   # files is a char array of all the csv files path
-  #files <- dir(directory, pattern = '.csv', full.names = TRUE)
+  files <- dir(directory, pattern = '.csv', full.names = TRUE)
   
   #apply read.csv to the list of all files and store data into var tables
-  #tables <- lapply(files, read.csv)
+  tables <- lapply(files, read.csv)
+  
+  #combine the large lists of tables into one and store in data var
+  assign('data',do.call(rbind, tables))
+  
+  #attach data so no need to add $ to refer to var
+  #attach(data)
+  
+  #return(tables)
+  
+  # subset data to the rows and column defined by user input
+  temp <- subset(data,data$ID >= id[1] & data$ID <= id[length(id)])
+  print(head(temp))
+  
+  pollutant <- sulfate
+  
+  temp <- temp$pollutant
+  
+  print(head(temp))
+  
+  # calculate mean without na value
+  res <- mean(temp,na.rm = TRUE)
+  
+  return(res)
   
   # combine all rows of tables into one
   #do.call(rbind, tables)
