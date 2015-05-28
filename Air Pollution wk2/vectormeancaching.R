ad1c10203ddf0d59c5767f81d9dfe47290b68a2d
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2015-05-28 16:16:02 -0400

    commented vectormeancaching.R in details in each step

--- Air Pollution wk2/vectormeancaching.R
+++ Air Pollution wk2/vectormeancaching.R
@@ -1,26 +1,62 @@
+#Usage example :
+#t <- makeVector(10:15)
+#t1 <- cachemean(t)
+
+# function to create a list of functions
 makeVector <- function(x = numeric()) {
+  
+  # m is the var holds mean, is set NULL as default value 
   m <- NULL
+  
+  # define set function
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  
+  # define get function
   get <- function() x
+  
+  # define setmean function, assigns mean to m and return
+  # super assignment operator <<- is used, which starts the closing framework, works it way up to the global environment
+  # until it finds a variable called mean 
+  # super assignment operator can only be used in functions
   setmean <- function(mean) m <<- mean
+  
+  # define getmean function, returns m var
   getmean <- function() m
+  
+  # create the list with names are set, get, setmean, getmean
+  # and assign the functions to the names
   list(set = set, get = get,
        setmean = setmean,
        getmean = getmean)
 }
 
 
+# function to get mean from cache or calculate
 cachemean <- function(x, ...) {
+  
+  # get the mean value from x
   m <- x$getmean()
+  
+  # if mean is already calculated, then it will not be a null
   if(!is.null(m)) {
     message("getting cached data")
+    
+    # return cached mean, once return, this function ends
     return(m)
   }
+  
+  # else get the vector that is the input of makeVector
   data <- x$get()
+  
+  # calculate mean and store it into var m
   m <- mean(data, ...)
+  
+  # set the mean in setmean which uses a super assignment <<-
   x$setmean(m)
+  
+  # return calculate mean value
   m
 }
